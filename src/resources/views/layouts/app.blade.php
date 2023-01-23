43054bfb46b1324078f3c864cfcdebc7eeb45c52
andresfb <andres@bastidas.us>
2023-01-22 21:49:57 -0500

    Added the side menu to the layout

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,7 +8,10 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+{{--        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">--}}
+{{--        <link rel="preconnect" href="https://fonts.googleapis.com">--}}
+{{--        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>--}}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap&text=daisyUIThemostpopular,freeandopen-sourceTailwindCSScomponentlibrary" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -16,26 +19,111 @@
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-base-100">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+        <div class="drawer drawer-mobile">
+            <input id="drawer" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content flex flex-col items-center justify-center">
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                @livewire('navigation-menu')
+
+                <!-- Page content here -->
+
+                <main>
+                    {{ $slot }}
+                </main>
+
+            </div>
+            <div class="drawer-side">
+                <label for="drawer" class="drawer-overlay"></label>
+                <aside class="bg-base-200 w-80">
+                    <div class="z-20 bg-base-200 bg-opacity-90 backdrop-blur sticky top-0 items-center gap-2 px-4 py-2 hidden lg:flex">
+                        <a href="{{ route('dashboard') }}" aria-current="page" aria-label="Dashboard" class="flex-0 btn btn-ghost px-2">
+                            <div class="inline-flex transition-all duration-200 md:text-3xl">
+                                <span class="mt-2 mr-2 text-primary">
+                                    <svg viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8">
+                                        <path fill-rule="evenodd" d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                                <span class="text-gray-700">{{ config('app.name') }}</span>
+                            </div>
+                        </a>
                     </div>
-                </header>
-            @endif
+                    <!-- Sidebar content here -->
+                    <ul class="menu menu-compact flex flex-col p-0 px-4 space-y-4">
+                        <li></li>
+                        <li class="menu-title">
+                            <span>Main Menu</span>
+                        </li>
+                        <li>
+                            <a href="{{ route('dashboard') }}" id="marks" class="flex gap-4 active">
+                                <span class="flex-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                      <path d="M11.644 1.59a.75.75 0 01.712 0l9.75 5.25a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.712 0l-9.75-5.25a.75.75 0 010-1.32l9.75-5.25z" />
+                                      <path d="M3.265 10.602l7.668 4.129a2.25 2.25 0 002.134 0l7.668-4.13 1.37.739a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.71 0l-9.75-5.25a.75.75 0 010-1.32l1.37-.738z" />
+                                      <path d="M10.933 19.231l-7.668-4.13-1.37.739a.75.75 0 000 1.32l9.75 5.25c.221.12.489.12.71 0l9.75-5.25a.75.75 0 000-1.32l-1.37-.738-7.668 4.13a2.25 2.25 0 01-2.134-.001z" />
+                                    </svg>
+                                </span>
+                                <span class="flex-1">Bookmarks</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" id="tags" class="flex gap-4">
+                                <span class="flex-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                      <path fill-rule="evenodd" d="M11.097 1.515a.75.75 0 01.589.882L10.666 7.5h4.47l1.079-5.397a.75.75 0 111.47.294L16.665 7.5h3.585a.75.75 0 010 1.5h-3.885l-1.2 6h3.585a.75.75 0 010 1.5h-3.885l-1.08 5.397a.75.75 0 11-1.47-.294l1.02-5.103h-4.47l-1.08 5.397a.75.75 0 01-1.47-.294l1.02-5.103H3.75a.75.75 0 110-1.5h3.885l1.2-6H5.25a.75.75 0 010-1.5h3.885l1.08-5.397a.75.75 0 01.882-.588zM10.365 9l-1.2 6h4.47l1.2-6h-4.47z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                                <span class="flex-1">Tags</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" id="archived" class="flex gap-4">
+                                <span class="flex-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                      <path d="M5.507 4.048A3 3 0 017.785 3h8.43a3 3 0 012.278 1.048l1.722 2.008A4.533 4.533 0 0019.5 6h-15c-.243 0-.482.02-.715.056l1.722-2.008z" />
+                                      <path fill-rule="evenodd" d="M1.5 10.5a3 3 0 013-3h15a3 3 0 110 6h-15a3 3 0 01-3-3zm15 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm2.25.75a.75.75 0 100-1.5.75.75 0 000 1.5zM4.5 15a3 3 0 100 6h15a3 3 0 100-6h-15zm11.25 3.75a.75.75 0 100-1.5.75.75 0 000 1.5zM19.5 18a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" clip-rule="evenodd" />
+                                    </svg>
+                                </span>
+                                <span class="flex-1">Archived</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" id="hidden" class="flex gap-4">
+                                <span class="flex-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                      <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                                      <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                                      <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                                    </svg>
+                                </span>
+                                <span class="flex-1">Hidden</span>
+                            </a>
+                        </li>
+                    </ul>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                    <ul class="menu menu-compact flex flex-col p-0 px-4 space-y-2">
+                        <li></li>
+                        <li class="menu-title">
+                            <span>Sections</span>
+                        </li>
+                        <li>
+                            <a href="#" id="general" class="flex gap-4">
+                                General
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" id="general" class="flex gap-4">
+                                For Work
+                            </a>
+                        </li>
+                    </ul>
+                </aside>
+
+            </div>
         </div>
+
 
         @stack('modals')
 
