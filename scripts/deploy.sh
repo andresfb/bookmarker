6e590d0e013f64798ba1973e4ee94bac4d55fcb5
andresfb <andres@bastidas.us>
2023-02-17 18:24:06 -0500

    added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SOURCE_PATH=./
+EXCLUDE_FILE=exclude.txt
+DESTINATION_PATH=/data1/bookmarker
+
+echo "Syncing code..."
+rsync -ravuh --delete --progress --exclude-from $EXCLUDE_FILE $SOURCE_PATH $DESTINATION_PATH
